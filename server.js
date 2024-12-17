98b6cdcf6d4efd98ce47fb527e40b8b6b8b5b40d
kopiibara <jcarljames123@gmail.com>
2024-12-18 05:59:16 +0800

    final commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
-
-import express from 'express';
-
+const express = require("express");
 const path = require("path");
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
